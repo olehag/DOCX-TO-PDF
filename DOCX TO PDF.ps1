0b08bace7c98bfde6d8c031cc35e61008368b2b1
Lord Hagen <omhagen97@gmail.com>
2020-11-29 16:05:17 +0100

    Update DOCX TO PDF.ps1

--- DOCX TO PDF.ps1
+++ DOCX TO PDF.ps1
@@ -1,6 +1,3 @@
-#Lord Hagen
-#Olehag04@fnk.no
-
 # if adminaccess is needed. uncomment line below.
 #if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
